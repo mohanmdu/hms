89e4e326c6cffdbf952cfbd84ec4ac6f192e2787
mohanmdu <mohanraj2014@gmail.com>
2024-12-28 19:00:17 +0530

    ui changes

--- rental-ui/rental-ui/src/app/component/rental/user/user.component.ts
+++ rental-ui/rental-ui/src/app/component/rental/user/user.component.ts
@@ -7,7 +7,6 @@ import { User } from '../../model/user';
 import { ToastrService } from 'ngx-toastr';
 import { RentalService } from '../rental-service';
 
-
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
